a6e5b11b8f7345384f9ac7553a9298b35c250dcc
thanbv1510 <thanbv1510@gmail.com>
2021-07-07 14:13:37 +0700

    Practice coding 2021/07/07

---
+++ LeetCode/NumberOfGoodPairs.java
@@ -0,0 +1,22 @@
+/**
+ * https://leetcode.com/problems/number-of-good-pairs/
+ */
+
+class Solution {
+    public int numIdenticalPairs(int[] nums) {
+        Map<Integer, Integer> data = new HashMap<>();
+
+        int total = 0;
+        for (int i = 0; i < nums.length; i++) {
+            int orDefault = data.getOrDefault(nums[i], 0);
+
+            if (orDefault != 0) {
+                total += orDefault;
+            }
+
+            data.put(nums[i], ++ orDefault);
+        }
+
+        return total;
+    }
+}
